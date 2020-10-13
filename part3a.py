6f84578e5ec7c37684894f8427bb226c8d97a39c
sira7740 <55516772+sira7740@users.noreply.github.com>
2020-10-13 15:16:03 -0700

    Add files via upload

---
+++ part3a.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import time
+from pprint import pprint
+
+import googleapiclient.discovery
+import google.auth
+import google.oauth2.service_account as service_account
+
+#
+# Use Google Service Account - See https://google-auth.readthedocs.io/en/latest/reference/google.oauth2.service_account.html#module-google.oauth2.service_account
+#
+credentials = service_account.Credentials.from_service_account_file(filename='service-credentials.json')
+project = os.getenv('GOOGLE_CLOUD_PROJECT') or 'dc-99999'
+service = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
+
+bucket = 'dc-sitesh'
+zone = 'us-west1-b'
+disk = 'demo-2'
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items'] if 'items' in result else None
+
+def create_instance(compute, project, zone, name, bucket):
+    # Get the latest Debian Jessie image.
+    image_response = compute.images().getFromFamily(
+        project='ubuntu-os-cloud', family='ubuntu-1804-lts').execute()
+    source_disk_image = image_response['selfLink']
+
+    # Configure the machine
+    machine_type = "zones/%s/machineTypes/f1-micro" % zone
+    startup_script = open(
+        os.path.join(
+            os.path.dirname(__file__), 'startup-script1.sh'), 'r').read()
+    image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+    image_caption = "Ready for dessert?"
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write',
+                'https://www.googleapis.com/auth/cloud-platform'
+            ]
+        }],
+        'metadata': {
+            'items': [{
+                'key': 'startup-script',
+                'value': startup_script
+            }, {
+                'key': 'url',
+                'value': image_url
+            }, {
+                'key': 'text',
+                'value': image_caption
+            }, {
+                'key': 'bucket',
+                'value': bucket
+            }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+# [END create_instance]
+
+
+# [START delete_instance]
+def delete_instance(compute, project, zone, name):
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
+# [END delete_instance]
+
+
+# [START wait_for_operation]
+def wait_for_operation(compute, project, zone, operation):
+    print('Waiting for operation to finish...')
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            print("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
+# [END wait_for_operation]
+
+
+# [START run]
+def main(project, bucket, zone, instance_name, wait=True):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    print('Creating instance.')
+
+    operation = create_instance(compute, project, zone, instance_name, bucket)
+    wait_for_operation(compute, project, zone, operation['name'])
+
+    instances = list_instances(compute, project, zone)
+
+    print('Instances in project %s and zone %s:' % (project, zone))
+    for instance in instances:
+        print(' - ' + instance['name'])
+
+
+main(project, bucket, zone, 'via-service-1')
